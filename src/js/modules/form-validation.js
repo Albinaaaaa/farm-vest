99a20da5a0b8a0b047f39e4bd072de0c26461c03
Albina <albinarubanchuk@gmail.com>
2022-11-07 15:38:22 +0200

    add download block and footer html, css, js

--- src/js/modules/form-validation.js
+++ src/js/modules/form-validation.js
@@ -1,6 +1,15 @@
 const form = document.forms.loginForm;
 const input = form.elements.loginEmail;
-const emailError = document.querySelector(".form__error");
+const emailError = document.querySelectorAll(".form__error");
+
+console.log(emailError[1]);
+
+const form2 = document.forms.loginForm2;
+const input2 = form2.elements.loginEmail2;
+
+// form2.addEventListener("click", function (e) {
+//   console.log(e);
+// });
 
 function validateEmail(element) {
   const email = element.value;
@@ -22,34 +31,80 @@ function hideError(element) {
 
 form.addEventListener("submit", function (e) {
   if (!validateEmail(input)) {
-    showError(emailError);
+    showError(emailError[0]);
     preventDefault();
   } else {
-    hideError(emailError);
+    hideError(emailError[0]);
   }
 });
 
 input.addEventListener("focus", function (e) {
   if (!validateEmail(input)) {
-    showError(emailError);
+    showError(emailError[0]);
   } else {
-    hideError(emailError);
+    hideError(emailError[0]);
   }
 });
 
 input.addEventListener("blur", function (e) {
   if (!validateEmail(input)) {
-    showError(emailError);
+    showError(emailError[0]);
   } else {
-    hideError(emailError);
+    hideError(emailError[0]);
   }
 });
 
 input.addEventListener("input", function (e) {
   console.log(e.target.value);
   if (!validateEmail(e.target)) {
-    showError(emailError);
+    showError(emailError[0]);
   } else {
-    hideError(emailError);
+    hideError(emailError[0]);
+  }
+});
+
+////////// form2
+
+function showError2(element) {
+  element.style.display = "block";
+  form2.style.border = "3px solid rgb(190, 12, 12)";
+}
+
+function hideError2(element) {
+  element.style.display = "none";
+  form2.style.border = "3px solid rgba(35, 177, 35, 1)";
+}
+
+form2.addEventListener("submit", function (e) {
+  if (!validateEmail(input2)) {
+    showError2(emailError[1]);
+    preventDefault();
+  } else {
+    hideError2(emailError[1]);
+  }
+});
+
+input2.addEventListener("focus", function (e) {
+  if (!validateEmail(input2)) {
+    showError2(emailError[1]);
+  } else {
+    hideError2(emailError[1]);
+  }
+});
+
+input2.addEventListener("blur", function (e) {
+  if (!validateEmail(input2)) {
+    showError2(emailError[1]);
+  } else {
+    hideError2(emailError[1]);
+  }
+});
+
+input2.addEventListener("input", function (e) {
+  console.log(e.target.value);
+  if (!validateEmail(e.target)) {
+    showError2(emailError[1]);
+  } else {
+    hideError2(emailError[1]);
   }
 });
